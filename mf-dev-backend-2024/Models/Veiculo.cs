b2708000f88735a728d21be36133ca80e2e946b5
alessandraMurat <alessandra.murat@outlook.com>
2024-04-13 22:36:24 -0300

    Criação da classe veículo

---
+++ mf-dev-backend-2024/Models/Veiculo.cs
@@ -0,0 +1,11 @@
+﻿namespace mf_dev_backend_2024.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Placa { get; set;}
+        public int AnoFabricação { get; set; }
+        public int AnoModelo { get; set; }
+    }
+}
